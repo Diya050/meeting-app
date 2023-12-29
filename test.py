5cbbf46abd84f2f301bac4e13e49b260719ea833
Diya050 <124448340+Diya050@users.noreply.github.com>
2023-12-29 14:22:48 +0530

    Update test.py

--- test.py
+++ test.py
@@ -32,19 +32,15 @@ def send_minutes(meeting):
 	else:
 		frappe.msgprint(_("Meeting Status must be 'Completed'"))
 
-
-
+  
 @frappe.whitelist()
 def start_meeting_message(meeting):
     meeting = frappe.get_doc("Meeting", meeting)
-
     if meeting.status == "Invitation Sent":
         current_time = now_datetime()
         meeting_from_time = get_datetime(str(meeting.date) + ' ' + str(meeting.from_time))  # Convert to str
-
         if current_time > meeting_from_time:
             delay_seconds = (current_time - meeting_from_time).seconds
-
             if -120 <= delay_seconds <= 300:  # Tolerance window: -2 minutes to 5 minutes
                 message = "Meeting is starting on time."
             else:
@@ -52,7 +48,6 @@ def start_meeting_message(meeting):
                 message = f"The meeting is starting {delay_minutes:.1f} minutes late. Meeting has started."
         else:
             message = "Meeting has started."
-
         for attendee in meeting.attendees:
             frappe.publish_realtime(event="meeting_started", message=message, user=attendee.attendee)
             
@@ -63,22 +58,17 @@ def start_meeting_message(meeting):
                 subject="Meeting Started: " + meeting.title,
                 message=message
             )
-
-
         meeting.status = "In Progress"
         meeting.save()
         frappe.msgprint(_("Meeting Status updated to 'In Progress'"))
     else:
         frappe.msgprint(_("Meeting Status must be 'Invitation Sent' to start the meeting"))
-
     	
     	
-
 @frappe.whitelist()
 def get_meetings(start, end):
 	if not frappe.has_permission("Meeting", "read"):
 		raise frappe.PermissionError
-
 	return frappe.db.sql("""select
 		timestamp(`date`, from_time) as start,
 		timestamp(`date`, to_time) as end,
