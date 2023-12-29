12840760e1ee28b021cd4faca15eb2e9930ce618
Diya050 <124448340+Diya050@users.noreply.github.com>
2023-12-29 14:19:35 +0530

    test

--- test.py
+++ test.py
@@ -1,32 +1,34 @@
 @frappe.whitelist()
-def send_invitation_emails(meeting):
-	meeting = frappe.get_doc("Meeting", meeting)
-	sender_fullname = get_fullname(frappe.session.user)
-	
-	if meeting.status == "Planned":
-		if meeting.attendees:
-				message = frappe.get_template("templates/emails/meeting_invitation.html").render({
-					"sender":sender_fullname,
-					"date":meeting.date,
-					"from_time":meeting.from_time,
-					"to_time":meeting.to_time,
-					"invitation_message":meeting.invitation_message,
-					"agenda": meeting.agenda,
-					"supplementary_agenda": meeting.supplementary_agenda,
-					"by_chairman_permission": meeting.by_chairman_permission,
-				})
-				frappe.sendmail(
-				recipients=[d.attendee for d in meeting.attendees],
-				sender=frappe.session.user,
-				subject="New Meeting:" + meeting.title,
-				message=message,
-				reference_doctype=meeting.doctype,
-				reference_name=meeting.name,
-				)
-				meeting.status = "Invitation Sent"
-				meeting.save()
-				frappe.msgprint(_("Invitation Sent"))
-		else:
-			frappe.msgprint("Enter atleast one Attendee for Sending")
-	else:
-		frappe.msgprint(_("Meeting Status must be 'Planned'"))
+def end_meeting_message(meeting):
+    meeting = frappe.get_doc("Meeting", meeting)
+
+    if meeting.status == "In Progress":
+        try:
+            message = "Meeting has ended."
+
+            for attendee in meeting.attendees:
+                frappe.publish_realtime(event="meeting_ended", message=message, user=attendee.attendee)
+
+            for attendee in meeting.attendees:
+                frappe.sendmail(
+                    recipients=[attendee.attendee],
+                    sender=frappe.session.user,
+                    subject="Meeting Ended: " + meeting.title,
+                    message=message
+                )
+
+            print("Before Status Update:", meeting.status)  # Add this line
+
+            meeting.status = "Completed"
+            meeting.save()
+            frappe.db.commit()
+            print("Commit Successful")
+
+
+            print("After Status Update:", meeting.status)  # Add this line
+
+            frappe.msgprint(_("Meeting Status updated to 'Completed'"))
+        except Exception as e:
+            frappe.msgprint(_("Error updating meeting status: {0}".format(str(e))))
+    else:
+        frappe.msgprint(_("Meeting Status must be 'In Progress' to end the meeting"))
